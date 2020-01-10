9c006e5f3347900e48963f8e3a05e06c263b0c05
Elshakoushy <melshakoushy@hotmail.com>
2020-01-10 17:51:56 +0200

    App setup and basic UI

---
+++ News App/View/NewsCell/NewsCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsCell.swift
+//  News App
+//
+//  Created by Mahmoud Elshakoushy on 1/10/20.
+//  Copyright © 2020 Mahmoud Elshakoushy. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
