a40d693f27737cd1a1cd89ac1a0313185918876e
Elshakoushy <melshakoushy@hotmail.com>
2020-01-10 05:59:51 +0200

    Initial Commit

---
+++ News App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by Mahmoud Elshakoushy on 1/10/20.
+//  Copyright © 2020 Mahmoud Elshakoushy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
